1809fe4952b98e179a18da276dcc5320f513b195
esakia <esakia@vt.edu>
2021-07-06 20:46:18 -0400

    getting this repo ready

--- HW4angular_starter/src/app/settings/settings.component.ts
+++
@@ -1,9 +0,0 @@
-
-
-
-
-
-
-
-
-//TODO: since you will be creating the 'settings' component from scratch, you should delete the folder containing the component files (css,html and ts) and do 'ng g c settings --skipTests'.
